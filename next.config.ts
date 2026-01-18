b66d9747e0e0f3ded723d3edf6a78e22873fd01a
jeonghwan <jeonghwan5790@outlook.kr>
2026-01-19 02:15:44 +0900

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  //output: "export", // 주석 해제하여 정적 빌드 활성화 (토스 업로드용)
+  //output: "export",      // 정적 빌드 활성화
+  distDir: ".next/web",  // [중요] 토스 빌드 도구가 파일을 찾을 수 있게 경로 강제 지정
   images: {
-    unoptimized: true, // 정적 배포 시 이미지 최적화 기능 비활성화 필수
+    unoptimized: true,   // 정적 배포 시 필수
   },
 };
 
