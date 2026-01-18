462c117993d5487f8347d440415bbdd99f6e3d03
jeonghwan <jeonghwan5790@outlook.kr>
2026-01-19 02:16:57 +0900

    fix: disable export for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  //output: "export",      // 정적 빌드 활성화
-  distDir: ".next/web",  // [중요] 토스 빌드 도구가 파일을 찾을 수 있게 경로 강제 지정
+  // output: "export",      // <--- Vercel 배포할 때는 이 줄을 반드시 주석 처리!
+  // distDir: ".next/web",  // <--- 이 줄도 Vercel에서는 필요 없으니 주석 처리!
   images: {
-    unoptimized: true,   // 정적 배포 시 필수
+    unoptimized: true,
   },
 };
 
